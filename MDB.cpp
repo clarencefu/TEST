796f1a34c4f139b9821cf1d61d1f8836e4b35768
clarencefu <stan0330@gmail.com>
2018-04-17 14:23:01 +0800

    新增註解

--- MDB.cpp
+++ MDB.cpp
@@ -12,6 +12,6 @@ class Solution
     public:
     int maxDepth(TreeNode* root) 
     {
-        return (root == NULL ? 0 : max(maxDepth(root -> left), maxDepth(root -> right)) + 1 );
+        return (root == NULL ? 0 : max(maxDepth(root -> left), maxDepth(root -> right)) + 1 );//使用recursive
     }
 };
